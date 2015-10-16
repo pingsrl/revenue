2a610bc08615c6760e9631cd112015860494fe84
Giacomo Trezzi <giacomo.trezzi@gmail.com>
2015-10-16 03:34:25 +0200

    added net and gross options

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ const year = new Date().getFullYear();
 
 program
   .version(pkg.version)
+  .command('revenue')
   .option('-y, --year [year]', 'specify a year different than ' + year, year)
+  .option('-n, --net', 'net only')
+  .option('-g, --gross', 'gross only')
   .option('-q, --quarter [quarter]', 'specify a quarter (1,2,3,4) or just -q for all', /^(1|2|3|4)$/i)
   .parse(process.argv);
 

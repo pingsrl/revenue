f5e05da4fd655c9b50a55b7284ecb92349fe866c
Giacomo Trezzi <giacomo.trezzi@gmail.com>
2015-10-16 03:19:15 +0200

    cache and quarters

--- README.md
+++ README.md
@@ -13,18 +13,43 @@ Revenue is a tool that gives you a quick summary of your revenue from [Harvest](
         Gross sales: 122,000.00 €
         Net sales: 100,000.00 €
 
-    $ revenue -y 2014
+    $ revenue -y 2012
 
-    Revenue 2014
+    Revenue 2012
         Gross sales: 61,000.00 €
         Net sales: 50,000.00 €
+
+    $ revenue -qy 2014
+
+    Revenue 2014
+    Q1  Gross sales: 30,5000.00 €
+      Net sales: 25,000.00 €
+
+    Q2  Gross sales: 30,5000.00 €
+      Net sales: 25,000.00 €
+
+    Q3  Gross sales: 30,5000.00 €
+      Net sales: 25,000.00 €
+
+    Q4  Gross sales: 30,5000.00 €
+      Net sales: 25,000.00 €
+
+    TOTAL Gross sales: 122,000.00 €
+      Net sales: 100,000.00 €
+
+
+    $ revenue -q 2 -y 2014
+
+    Revenue 2014
+    Q2  Gross sales: 30,5000.00 €
+      Net sales: 25,000.00 €
 
 
 # TODO
 
-* cache
-* locales
-* quarters
-* comparisons
-* graphs
-* estimates
+    * [x] cache
+    * [ ] locales
+    * [x] quarters
+    * [ ] comparisons
+    * [ ] graphs
+    * [ ] estimates
--- index.js
+++ index.js
@@ -1,36 +1,16 @@
 #! /usr/bin/env node
 var program = require('commander');
-var fs = require('fs');
-var path = require('path');
-var invoices = require('./lib/invoices');
 
-function getConfigPath() {
-  return path.resolve(process.env[(process.platform === 'win32') ? 'USERPROFILE' : 'HOME'] +'/.fatturato');
-}
+var updateNotifier = require('update-notifier');
+var pkg = require('./package.json');
+updateNotifier({pkg: pkg, updateCheckInterval:1}).notify();
+
+const year = new Date().getFullYear();
 
 program
-  .version('1.0.0')
-  .option('-y, --year [year]', 'specify a year different than [year]' , new Date().getFullYear())
-  // .option('-q, --quarter <quarter>', 'specify a quarter (1,2,3,4)', /^(1|2|3|4)$/i , '1')
+  .version(pkg.version)
+  .option('-y, --year [year]', 'specify a year different than ' + year, year)
+  .option('-q, --quarter [quarter]', 'specify a quarter (1,2,3,4) or just -q for all', /^(1|2|3|4)$/i)
   .parse(process.argv);
 
-var config;
-try {
-  config = JSON.parse(fs.readFileSync(getConfigPath()));
-}
-catch (e) {
-  var prompt = require('prompt');
-  prompt.start();
-  prompt.get(['subdomain', 'email', 'password'], function (err, result) {
-    config = result;
-    if (config) {
-      fs.writeFileSync(getConfigPath(), JSON.stringify(config));
-      invoices(config, program);
-    }
-  });
-}
-finally{
-  if (config && program.year) {
-    invoices(config, program);
-  }
-}
+require('./lib/invoices')(program);

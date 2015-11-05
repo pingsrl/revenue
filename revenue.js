1f6610534ee52a370d47b7801b0926a65b3b6e31
Giacomo Trezzi <giacomo.trezzi@gmail.com>
2015-11-06 00:37:35 +0100

    using revenue-core library

--- revenue.js
+++ revenue.js
@@ -1,19 +1,35 @@
 #! /usr/bin/env node
 
 // update notifier
+var program = require('commander');
 var updateNotifier = require('update-notifier');
+var invoices = require('revenue-core');
+
+// Update notification
 var pkg = require('./package.json');
 updateNotifier({pkg: pkg, updateCheckInterval:1}).notify();
 
+// Setting up spinner
+var Spinner = require('cli-spinner').Spinner;
+var spinner = new Spinner('Loading data...this can take a while.. %s');
+spinner.setSpinnerString('|/-\\');
+
+// Current year
 const year = new Date().getFullYear();
 
-// CLI
-var program = require('commander');
+// CLI interface
 program
   .version(pkg.version)
   .option('-f, --force', 'force update')
   .option('-y, --year [year]', 'specify a year different than ' + year, year)
   .parse(process.argv);
 
-// actual code
-require('./lib/invoices')(program);
+var configurator = require('./lib/configator');
+var visualizer = require('./lib/visualizer');
+
+spinner.start();
+// Getting invoices
+invoices(program, configurator, (db, lastUpdate, options) => {
+  spinner.stop(true);
+  visualizer(db, lastUpdate, options);
+});

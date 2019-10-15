790d969f9ebe265cfe90b798d5b8c1ca82ebd036
Giacomo Trezzi <giacomo.trezzi@gmail.com>
2019-10-15 20:51:00 +0200

    aggiornato core

--- revenue.js
+++ revenue.js
@@ -1,16 +1,18 @@
 #! /usr/bin/env node
 
 // update notifier
+var pkg = require('./package.json');
 var updateNotifier = require('update-notifier');
 var program = require('commander');
 var revenue = require('revenue-core');
+var Spinner = require('cli-spinner').Spinner;
+var configurator = require('./lib/configator');
+var visualizer = require('./lib/visualizer');
 
 // Update notification
-var pkg = require('./package.json');
 updateNotifier({ pkg: pkg, updateCheckInterval: 1 }).notify();
 
 // Setting up spinner
-var Spinner = require('cli-spinner').Spinner;
 var spinner = new Spinner('Loading data...this can take a while.. %s');
 spinner.setSpinnerString('|/-\\');
 
@@ -23,9 +25,6 @@ program
 	.option('-f, --force', 'force update')
 	.option('-y, --year [year]', 'specify a year different than ' + year, year)
 	.parse(process.argv);
-
-var configurator = require('./lib/configator');
-var visualizer = require('./lib/visualizer');
 
 spinner.start();
 // Getting invoices
